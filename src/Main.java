45a6c1d1fc27fe5ec93424d1bcb09b06968dce9a
Marklee2703 <littlemark2703@gmail.com>
2023-06-20 16:28:22 +1200

    new code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
+import MyPackage.MyFirstClass;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("I am using my class");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        MyFirstClass myFirstObject = new MyFirstClass("Lei Song",10);
+        System.out.println(myFirstObject.getName());
+        System.out.println(myFirstObject.getAge());
+        myFirstObject.setAge(19);
+        System.out.println(myFirstObject.getName());
+        System.out.println(myFirstObject.getAge());
     }
 }
---
+++ src/MyPackage/MyFirstClass.java
@@ -0,0 +1,27 @@
+package MyPackage;
+
+public class MyFirstClass {
+    private String name;
+    private Integer age;
+
+    public MyFirstClass(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
